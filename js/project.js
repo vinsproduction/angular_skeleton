03427d9c7bc085a2c14b647cc5ceb791a0fe60fb
Vins Polyakov <vinsproduction@gmail.com>
2014-12-10 14:30:45 +0300

    min

--- js/project.js
+++ js/project.js
@@ -51,6 +51,31 @@ app.config(['$interpolateProvider', function($interpolateProvider) {}]);
 
 
 /* 
+ Router
+ */
+app.config([
+  '$routeProvider', function($routeProvider) {
+    $routeProvider.when('/', {
+      templateUrl: 'views/index.html',
+      controller: 'indexCtrl'
+    });
+    $routeProvider.when('/index', {
+      redirectTo: '/'
+    });
+    $routeProvider.otherwise({
+      templateUrl: 'views/404.html',
+      controller: function($rootScope) {
+        $rootScope.title = '404';
+        return $rootScope.bodyClass = 'view-404';
+      },
+      redirectTo: '/404'
+    });
+    return 0;
+  }
+]);
+
+
+/* 
  Controllers
  */
 var appControllers;
